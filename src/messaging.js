34bdeef1f58087bf228fc3ec5ab499261b1a4dae
Raman Singh <mail.raman.s@gmail.com>
2019-12-17 00:58:47 +0100

    Add support for firebase messaging

--- src/messaging.js
+++ src/messaging.js
@@ -1,6 +1,124 @@
+/*
+  Mock for admin.messaging.Messaging
+  https://firebase.google.com/docs/reference/admin/node/admin.messaging
+*/
 'use strict';
+var _ = require('./lodash');
+var assert = require('assert');
+var Promise = require('rsvp').Promise;
+var autoId = require('firebase-auto-ids');
+var Queue = require('./queue').Queue;
 
 function MockMessaging() {
+  this.results = {};
+  this.queue = new Queue();
+  this.flushDelay = false;
 }
+
+// https://firebase.google.com/docs/reference/admin/node/admin.messaging.Messaging.html#send
+MockMessaging.prototype.send = function(message, dryRun) {
+  assert(!_.isUndefined(message), 'message must not be undefined');
+  return this._send(_.toArray(arguments), 'send', function() {
+    var messageId = autoId(new Date().getTime());
+    console.log(typeof(result));
+    return messageId;
+  });
+};
+
+// https://firebase.google.com/docs/reference/admin/node/admin.messaging.Messaging.html#send-all
+MockMessaging.prototype.sendAll = function(messages, dryRun) {
+  assert(Array.isArray(messages), 'messages must be an "Array"');
+  var self = this;
+  return this._send(_.toArray(arguments), 'sendAll', function() {
+    return self._mapMessagesToBatchResponse(messages);
+  });
+};
+
+// https://firebase.google.com/docs/reference/admin/node/admin.messaging.Messaging.html#send-multicast
+MockMessaging.prototype.sendMulticast = function(multicastMessage, dryRun) {
+  assert(!_.isUndefined(multicastMessage), 'multicastMessage must not be undefined');
+  assert(Array.isArray(multicastMessage.tokens));
+  var self = this;
+  return this._send(_.toArray(arguments), 'sendMulticast', function() {
+    return self._mapMessagesToBatchResponse(multicastMessage.tokens);
+  });
+};
+
+MockMessaging.prototype.flush = function(delay) {
+  this.queue.flush(delay);
+  return this;
+};
+
+MockMessaging.prototype.autoFlush = function (delay) {
+  if (_.isUndefined(delay)) {
+    delay = true;
+  }
+
+  this.flushDelay = delay;
+
+  return this;
+};
+
+MockMessaging.prototype.failNext = function(methodName, err) {
+  assert(err instanceof Error, 'err must be an "Error" object');
+  this.results[methodName] = err;
+};
+
+MockMessaging.prototype.nextResult = function(methodName, result) {
+  assert(!_.isUndefined(result), 'result must not be undefined');
+  assert(!(result instanceof Error), 'result must not be an "Error" object');
+  this.results[methodName] = result;
+};
+
+MockMessaging.prototype._send = function(args, methodName, defaultResultFn) {
+  var result = this._nextResult(methodName);
+  var self = this;
+  return new Promise(function (resolve, reject) {
+    self._defer(methodName, args, function () {
+      if (result === null) {
+        resolve(defaultResultFn());
+      } else if (result instanceof Error) {
+        reject(result);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+  
+MockMessaging.prototype._nextResult = function(type) {
+  var err = this.results[type];
+  delete this.results[type];
+  return err || null;
+};
+
+MockMessaging.prototype._defer = function(sourceMethod, sourceArgs, callback) {
+  this.queue.push({
+    fn: callback,
+    context: this,
+    sourceData: {
+      ref: this,
+      method: sourceMethod,
+      args: sourceArgs
+    }
+  });
+  if (this.flushDelay !== false) {
+    this.flush(this.flushDelay);
+  }
+};
+
+MockMessaging.prototype._mapMessagesToBatchResponse = function(messages) {
+  return {
+    failureCount: 0,
+    successCount: messages.length,
+    responses: messages.map(function(message) {
+      return {
+        error: undefined,
+        messageId: autoId(new Date().getTime()),
+        success: true,
+      };
+    })
+  };
+};
 
 module.exports = MockMessaging;
